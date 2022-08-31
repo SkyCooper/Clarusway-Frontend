74e4b011feba754a59d0a9a28a95e00e9573b33c
Cooper <dancersoftaranto@gmail.com>
2022-08-31 19:21:34 +0300

    soru cözümü

--- frontend/javascript/sessions/s00_soruCozumleri/mustafa31Agu.js
+++ frontend/javascript/sessions/s00_soruCozumleri/mustafa31Agu.js
@@ -87,30 +87,52 @@
 //! Q-6
 //? Write a code that prints a multiplication table of numbers 1 to 10.
 
-const multiTable = () => {
-  let arr = [];
-  for (let i = 1; i <= 10; i++) {
-    arr.push(`multiplication table of ${i}`);
-    for (let j = 1; j <= 10; j++) {
-      arr.push(`${i} x ${j} = ${i * j}`);
-    }
-  }
-  return arr;
-};
+// const multiTable = () => {
+//   let arr = [];
+//   for (let i = 1; i <= 10; i++) {
+//     arr.push(`multiplication table of ${i}`);
+//     for (let j = 1; j <= 10; j++) {
+//       arr.push(`${i} x ${j} = ${i * j}`);
+//     }
+//   }
+//   return arr;
+// };
 
-console.log(multiTable());
+// console.log(multiTable());
 
 //! Q-7
 //? Write a code to draw Whiteball and Powerball numbers for the Powerball lottery game.
 //? 5 Whiteball numbers are chosen from 01 to 69 and 1 Powerball number is chosen from 1 to 26.
 
+// const powerball = () => {
+//     let powerball = Math.floor(Math.random() * 26) + 1
+//     let whiteball = []
+
+//     for (let i = 1; i <= 5; i++){
+//         let random = Math.floor(Math.random() * 69) + 1
+//         whiteball.push(random)
+//     }
+
+//     return `${whiteball.join(', ')}, ${powerball}`
+// }
+
+// console.log(powerball())
+
 //* 2. Bölüm Functions
 
 //! Q-1
 //? Write a Function that takes 2 parameters (amount and rate) to convert currency.
+//* süslü kullanılmazsa return gerek yok,
+//* default 1.5, girilmez
+// const currency = (amount, rate = 1.5) => amount * rate;
+
+// console.log(currency(1000, 1.7));
 
 //! Q-2
 //? Write a JavaScript function that accepts an argument and returns the type.
+
+// const type = (x) => typeof x;
+// console.log(type(true));
 
 //! Q-3
 //? You are given a string and character. Write a function to check the string and find how many
@@ -118,10 +140,46 @@ console.log(multiTable());
 //? Example:
 //? str = "Clarusway"         char = "a"       ===> result = 2 as "a" occurs 2 times in Clarusway
 
+const times = (str, char) => {
+  let count = 0;
+  for (let i = 0; i < str.length; i++) {
+    // str[i] === char && count++   //!shortend sol taraf true ise sağ taraf çalışıp artsın
+    if (str[i] == char) {
+      count++;
+    }
+  }
+  return `${char} occurs ${count} times in ${str}`;
+};
+console.log(times("Clarusway", "a"));
+
+const times1 = (str, char) => {
+  return str.split(char).length - 1;
+};
+console.log(times1("Clarusway", "a"));
+
 //! Q-4
 //? Write a JS code to display Perfect Numbers from 1 to 1000.
 //? A perfect number is a positive integer that is equal to the sum of its positive divisors, excluding the number itself.
 //? For instance, 6 has divisors 1, 2 and 3 (excluding itself), and 1 + 2 + 3 = 6, so 6 is a perfect number.
+
+const perfectNums = () => {
+  let arr = [];
+
+  for (let i = 1; i <= 1000; i++) {
+    sum = 0;
+    for (let k = 1; k < i; k++) {
+      if (!(i % k)) {
+        sum += k;
+      }
+    }
+    if (sum === i) {
+      arr.push(i);
+    }
+  }
+  return arr;
+};
+
+console.log(perfectNums());
 
 //! Q-5
 //? Develop a guessing game. Randomly generate a number between 0-100. Ask the user to guess that number.
