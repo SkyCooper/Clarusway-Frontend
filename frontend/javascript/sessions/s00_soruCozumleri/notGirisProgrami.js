ae3e9883ebbc76236d76b1e1c4dddc57db5b127d
Cooper <dancersoftaranto@gmail.com>
2022-09-11 19:37:39 +0300

    edit

--- frontend/javascript/sessions/s00_soruCozumleri/notGirisProgrami.js
+++ frontend/javascript/sessions/s00_soruCozumleri/notGirisProgrami.js
@@ -45,47 +45,47 @@ while (true) {
   }
 }
 
-//! fonksiyonsuz çözüm;
+// //! fonksiyonsuz çözüm;
 
-let isimler = "";
-while (true) {
-  let menu = prompt(
-    "1 - Ogretmen menu\n2 - Ogrenci menu\n 0 - Exit\n(ogrenci menude notların gorünebilmesi icin ogretmen menuden notların girilmesi gerekir...)"
-  );
+// let isimler = "";
+// while (true) {
+//   let menu = prompt(
+//     "1 - Ogretmen menu\n2 - Ogrenci menu\n 0 - Exit\n(ogrenci menude notların gorünebilmesi icin ogretmen menuden notların girilmesi gerekir...)"
+//   );
 
-  if (menu == 0) {
-    break;
-  } else if (menu == 1) {
-    let sayac = +prompt("kac adet ogrenci verisi gireceksiniz : ");
-    let isim = "";
-    for (let i = 1; i <= sayac; i++) {
-      isim = prompt(`${i}. ogrenci ismini giriniz : `);
-      if (i == 1) {
-        isimler = isim + " = ";
-      } else {
-        isimler += "     " + isim + " = ";
-      }
-      let sayac2 = +prompt(`${isim} icin kac adet not gireceksiniz : `);
-      let not = 0;
-      let ort = 0;
-      let etki = 0;
-      for (let j = 1; j <= sayac2; j++) {
-        not = +prompt(`${j}. notu giriniz : `);
-        etki = +prompt(`bu not yuzde kac etkili(0-100 arası rakam yazınız) : `);
-        ort += (not * etki) / 100;
-      }
-      isimler += ort;
-      console.log("ogrenci isimleri ve notlari basariyla eklendi...");
-    }
-    // console.log(isimler);
-  }
-  if (menu == 2) {
-    let name = prompt("Lutfen aradıgınız ismi giriniz : ");
-    let index = isimler.search(name);
-    if (index != -1) {
-      console.log(isimler.slice(index, index + 6 + name.length));
-    } else {
-      console.log("kayit bulunamadi....");
-    }
-  }
-}
+//   if (menu == 0) {
+//     break;
+//   } else if (menu == 1) {
+//     let sayac = +prompt("kac adet ogrenci verisi gireceksiniz : ");
+//     let isim = "";
+//     for (let i = 1; i <= sayac; i++) {
+//       isim = prompt(`${i}. ogrenci ismini giriniz : `);
+//       if (i == 1) {
+//         isimler = isim + " = ";
+//       } else {
+//         isimler += "     " + isim + " = ";
+//       }
+//       let sayac2 = +prompt(`${isim} icin kac adet not gireceksiniz : `);
+//       let not = 0;
+//       let ort = 0;
+//       let etki = 0;
+//       for (let j = 1; j <= sayac2; j++) {
+//         not = +prompt(`${j}. notu giriniz : `);
+//         etki = +prompt(`bu not yuzde kac etkili(0-100 arası rakam yazınız) : `);
+//         ort += (not * etki) / 100;
+//       }
+//       isimler += ort;
+//       console.log("ogrenci isimleri ve notlari basariyla eklendi...");
+//     }
+//     // console.log(isimler);
+//   }
+//   if (menu == 2) {
+//     let name = prompt("Lutfen aradıgınız ismi giriniz : ");
+//     let index = isimler.search(name);
+//     if (index != -1) {
+//       console.log(isimler.slice(index, index + 6 + name.length));
+//     } else {
+//       console.log("kayit bulunamadi....");
+//     }
+//   }
+// }
