7fcb42aaf2be13429e502d087c853d63d601eefe
Cooper <dancersoftaranto@gmail.com>
2022-09-12 22:14:27 +0300

    event-example

--- frontend/javascript/sessions/s14_DOMevents/1-events-intro/events.js
+++ frontend/javascript/sessions/s14_DOMevents/1-events-intro/events.js
@@ -13,49 +13,86 @@ console.log("****** EVENTS *******");
 //* -------------------------------------------------------
 
 //? EVENTS yazısını seçip ona event tanımlayalım,
-const h1 = document.querySelector("header h1")
+const h1 = document.querySelector("header h1");
 console.log(h1); // doğru seçim yaptım mı? bakmak için
 
-
 //! 3'üncü yöntem
-h1.onmouseover = function(){
-    h1.style.color = "red";
-    h1.style.fontWeight = "300";
-    // üzendin çıkılınca tekrar düzeltmek veya başka bir özellik vermek gerekir.
-}
-h1.onmouseout = () =>{
-    h1.style.color = "black";
-    // h1.style.color = "blue"; 
-    h1.style.fontWeight = "900";
-}
+h1.onmouseover = function () {
+  h1.style.color = "red";
+  h1.style.fontWeight = "300";
+  // üzendin çıkılınca tekrar düzeltmek veya başka bir özellik vermek gerekir.
+};
+h1.onmouseout = () => {
+  h1.style.color = "black";
+  // h1.style.color = "blue";
+  h1.style.fontWeight = "900";
+};
 
 //! 4'üncü yöntem addEventListener ile yapımı;
-h1.addEventListener("click", ()=>{
-    alert("H1 clicked")
-})
+h1.addEventListener("click", () => {
+  alert("H1 clicked");
+});
 // h1 block element olduğundan kapladığı heryerde click ve diğer evenler çalışır.
-
 
 //* EXAMPLE-2 (addEventListener())
 //* -------------------------------------------------
- //? add butonuna tıklandığında içi boş ise uyarı versin, 
- //* önce buton seçilir.
- 
- document.getElementById("btn").addEventListener("click", function(){
-    const input = document.querySelector("#input")
-    // if(!input.value) { // input.value === null da yazılabilirdi.
-    //   alert("Please enter an item");
-    // }else{
-    //     alert(`${input.value} entered`)
-    // }
+//? add butonuna tıklandığında içi boş ise uyarı versin,
+//* önce buton seçilir.
 
-!input.value ? alert("Please enter an item") :  alert(`${input.value} entered`);
-// ternary yazılması
+document.getElementById("btn").addEventListener("click", function () {
+  const input = document.querySelector("#input");
+  // if(!input.value) { // input.value === null da yazılabilirdi.
+  //   alert("Please enter an item");
+  // }else{
+  //     alert(`${input.value} entered`)
+  // }
 
-input.value = ""; // input içinde değer kalmasın, silinsin diye
- } )  
+  !input.value
+    ? alert("Please enter an item")
+    : alert(`${input.value} entered`);
+  // ternary yazılması
+
+  input.value = ""; // input içinde değer kalmasın, silinsin diye
+});
 // sadece id aldığında # gerek yok, addEventListener olduğundan on- gerek yok
 
+//* EXAMPLE-3
+//* -------------------------------------------------
+//? liste elemanları üzerine gelince büyüsün sonra küçülsün.,
+
+const list = document.querySelectorAll(".list");
+console.log(list); // bütün li ler seçildi mi? nodelist olarak
+
+list.forEach((li) => {
+    // li.style.transition = "font-size 1s"  //burada css gibi yazıyoruz.
+    li.style.transition = "all 0.8s"  //burada css gibi yazıyoruz.
+    li.style.lineHeight = "2rem"
+
+  li.onmouseover = () => {
+    li.style.fontSize = "2rem";
+    li.style.transform = "translateX(10px)";
+  };
+
+  li.onmouseout = () => {
+    li.style.fontSize = "1rem";
+    // li.style.transform = "translateX(-10px)";
+
+  };
+
+});
+
+//! bunun kutu gibi olanını yap ders için..
 
 
- //? liste elemanları üzerine gelince büyüsün sonra küçülsün., 
+//* EXAMPLE-4
+//* -------------------------------------------------
+//? sayfa refresh yapıldığında input box active olsun / focus olsun,
+
+window.onload = function () {
+    document.querySelector("#input").focus();
+};
+
+
+//* EXAMPLE-5
+//* -------------------------------------------------
+//? sayfa refresh yapıldığında input box active olsun / focus olsun,
