3be3f8348da8143046a7168c8ac14202a9577623
Cooper <dancersoftaranto@gmail.com>
2022-09-12 21:25:08 +0300

    ders1

--- frontend/javascript/sessions/s14_DOMevents/1-events-intro/events.js
+++ frontend/javascript/sessions/s14_DOMevents/1-events-intro/events.js
@@ -2,14 +2,60 @@
 //*                        EVENTS
 //* ======================================================
 
+//! 2'nci yöntem
+const header = document.querySelector(".header2");
+const yazıMavi = () => (header.style.color = "blue");
+const yazıSiyah = () => (header.style.color = "black");
+
 console.log("****** EVENTS *******");
 
 //* EXAMPLE-1 (Mouse Over, Mouse Out )
 //* -------------------------------------------------------
 
+//? EVENTS yazısını seçip ona event tanımlayalım,
+const h1 = document.querySelector("header h1")
+console.log(h1); // doğru seçim yaptım mı? bakmak için
 
 
+//! 3'üncü yöntem
+h1.onmouseover = function(){
+    h1.style.color = "red";
+    h1.style.fontWeight = "300";
+    // üzendin çıkılınca tekrar düzeltmek veya başka bir özellik vermek gerekir.
+}
+h1.onmouseout = () =>{
+    h1.style.color = "black";
+    // h1.style.color = "blue"; 
+    h1.style.fontWeight = "900";
+}
+
+//! 4'üncü yöntem addEventListener ile yapımı;
+h1.addEventListener("click", ()=>{
+    alert("H1 clicked")
+})
+// h1 block element olduğundan kapladığı heryerde click ve diğer evenler çalışır.
 
 
 //* EXAMPLE-2 (addEventListener())
 //* -------------------------------------------------
+ //? add butonuna tıklandığında içi boş ise uyarı versin, 
+ //* önce buton seçilir.
+ 
+ document.getElementById("btn").addEventListener("click", function(){
+    const input = document.querySelector("#input")
+    // if(!input.value) { // input.value === null da yazılabilirdi.
+    //   alert("Please enter an item");
+    // }else{
+    //     alert(`${input.value} entered`)
+    // }
+
+!input.value ? alert("Please enter an item") :  alert(`${input.value} entered`);
+// ternary yazılması
+
+input.value = ""; // input içinde değer kalmasın, silinsin diye
+ } )  
+// sadece id aldığında # gerek yok, addEventListener olduğundan on- gerek yok
+
+
+
+ //? liste elemanları üzerine gelince büyüsün sonra küçülsün., 
