5c0755489a55dffacba95244b7485fe24e849e0d
Cooper <dancersoftaranto@gmail.com>
2022-09-14 09:34:49 +0300

    postclass

--- frontend/javascript/sessions/s14_DOMevents/0-deneme/deneme.js
+++ frontend/javascript/sessions/s14_DOMevents/0-deneme/deneme.js
@@ -1,32 +1,33 @@
 //! ÖRNEK-1
-// Butonumuza tıklandığında web sayfasının arkaplan renginin Yeşil olmasını ve
-// Butonun içindeki yazının BAS olarak değiştirilmesini sağlayan kodu yazalım.
+// Butonumuza tıklandığında web sayfasının arkaplan renginin buton iel aynı renk olmasını sağlayan event yazın
 
 const blueBtn = document.querySelector("button");
 const yellowBtn = document.getElementById("yellowBtn");
 const greenBtn = document.getElementsByClassName("btn-success");
+const body = document.getElementsByTagName("body")[0];
+const h1 = document.getElementsByTagName("h1")[0];
 
-// blueBtn.addEventListener("click", function() {
-//     document.getElementsByTagName("body")[0].style.background = "blue"
-//     document.getElementsByTagName("h1")[0].style.color = "white"
-// })
+// blueBtn.addEventListener("click", function () {
+//   document.getElementsByTagName("body")[0].style.background = "blue";
+//   document.getElementsByTagName("h1")[0].style.color = "white";
+// });
 
 // yellowBtn.addEventListener("click", function () {
-//     document.getElementsByTagName("body")[0].style.background = "yellow";
-//     document.getElementsByTagName("h1")[0].style.color = "black"
+//   body.style.background = "yellow";
+//   h1.style.color = "black";
 // });
 
 // greenBtn[0].addEventListener("click", function () {
-//   document.getElementsByTagName("body")[0].style.background = "green";
+//   body.style.background = "green";
 // });
 
-//! HTML-ONCLİCK ekleyerek yapma
+//! HTML-ONCLİCK -ONMOUSEOVER ekleyerek yapma
 // const colorChange = (event) => {if (event == "blueBtn") {
-//   document.getElementsByTagName("body")[0].style.background = "blue";
+//   body.style.background = "blue";
 // } else if (event == "yellowBtn") {
-//   document.getElementsByTagName("body")[0].style.background = "yellow";
+//   body.style.background = "yellow";
 // } else {
-//   document.getElementsByTagName("body")[0].style.background = "green";
+//   body.style.background = "green";
 // }};
 
 //! tek fonksiyon tanımlayıp yapma;
@@ -40,10 +41,14 @@ const greenBtn = document.getElementsByClassName("btn-success");
 //   }
 // }
 
+
 // blueBtn.addEventListener("click", colorChange);
 // yellowBtn.addEventListener("click", colorChange);
 // greenBtn[0].addEventListener("click", colorChange);
 
+
+
+//! ilave çözüm - tamamlanmadı, çalışmıyor.
 // function colorChange() {
 //   document.getElementsByTagName("body")[0].style.background =
 //     this.getAttribute("data");
@@ -54,37 +59,36 @@ const greenBtn = document.getElementsByClassName("btn-success");
 //   buttons[i].addEventListener("click", colorChange);
 // }
 
-console.log(document.getElementsByTagName("body"));
-console.log(document.getElementsByTagName("h1"));
+
 
 //!ÖRNEK-2
 // butona her basıldığında sayfanın arkaplan rengini rasgele olarak değiştiren uygulamayı yazınız.
 // NOT:Renkler(Mavi,Yeşil,Sarı,Siyah,Pembe v.b)dizide saklanacak Ve aktifolan renk h2 elamanı olarak ekranda yazdırılacaktır.
 
 const change = document.querySelector(".change");
-const h2 = document.getElementById("h2")
+const h2 = document.getElementById("h2");
 
 //! sabit bir diziden seçerek yapma
 // change.addEventListener("click", function () {
-//   const colors = ["blue", "red", "yellow", "black", "pink", "purple"];
+//   const colors = ["blue", "red", "yellow", "black", "pink", "purple", "aqua", "fuchia"];
 //   const randomNumber = Math.floor(Math.random() * colors.length);
+//   console.log(randomNumber);
 //   document.getElementsByTagName("body")[0].style.background =
 //     colors[randomNumber];
-//     h2.innerHTML = `Yeni değişen renk ${colors[randomNumber]}`
+//     h2.innerText = `Yeni değişen renk ${colors[randomNumber]}`
 //     if(colors[randomNumber] == "black"){
 //       h2.style.color = "white"
 //     }
 // });
 
-//!rastgele ürerilen sayı ile random renk yapma
+//!rastgele üretilen sayı ile random renk yapma
 // const codesArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
 // const randomNumber = () =>  Math.floor(Math.random() * codesArray.length);
 // console.log(randomNumber());
 
-
 // const randomColor = () => {
 //   let hexCode = "#";
-//   for (let x = 0; x < 6; x++) {
+//   for (let i = 0; i < 6; i++) {
 //     hexCode += codesArray[randomNumber()];
 //   }
 //   return hexCode;
@@ -92,21 +96,24 @@ const h2 = document.getElementById("h2")
 
 // change.addEventListener("click", function(){
 //   document.getElementsByTagName("body")[0].style.background = randomColor()
+//   h2.innerText = `HEX kodu ${randomColor()}`;
 // })
 
 //! örnek-3
 //  AÇ butonuna basıldığında lamba_on adındaki resmi,KAPA butonuna basıldığında ise lamba_off resmin gösteren uygulamayı yazalım
 
-const lambaOn = document.querySelector(".btnOn") 
-const lambaOff = document.querySelector(".btnOff") 
+const lambaOn = document.querySelector(".btnOn");
+const lambaOff = document.querySelector(".btnOff");
 console.log(lambaOff);
 
-lambaOn.addEventListener("click", function(){
-  document.querySelector(".on").classList.add("d-none")
-  document.querySelector(".off").classList.remove("d-none")
-})
+lambaOn.addEventListener("click", function () {
+  document.querySelector(".on").classList.add("d-none");
+  document.querySelector(".off").classList.remove("d-none");
+});
 
-lambaOff.addEventListener("click", function(){
-  document.querySelector(".on").classList.remove("d-none")
-  document.querySelector(".off").classList.add("d-none")
-})
+lambaOff.addEventListener("click", function () {
+  document.querySelector(".on").classList.remove("d-none");
+  document.querySelector(".off").classList.add("d-none");
+});
+
+console.log(document.querySelector(".on").classList);
