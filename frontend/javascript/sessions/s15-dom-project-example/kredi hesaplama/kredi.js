f04fa3bf8d367ed3a2aa9feb7352f239f3c7aac4
Cooper <dancersoftaranto@gmail.com>
2022-09-14 17:36:46 +0300

    kredi ödev son

--- frontend/javascript/sessions/s15-dom-project-example/kredi hesaplama/kredi.js
+++ frontend/javascript/sessions/s15-dom-project-example/kredi hesaplama/kredi.js
@@ -1,14 +1,14 @@
-const krediTipi = document.querySelector("#krediTipi").value;
-const vade = document.querySelector("#vade").value;
-const tutar = document.querySelector("#tutar").value;
 const buton = document.querySelector("button");
+const baslik = document.querySelector("#baslik");
+console.log(baslik);
 
 console.log(krediTipi);
 console.log(vade);
 console.log(tutar);
 console.log(buton);
 
-const faiz = () => {
+function faiz() {
+  let krediTipi = document.querySelector("#krediTipi").value;
   let faizOrani;
   if (krediTipi == "konut") {
     faizOrani = 1.68;
@@ -20,16 +20,40 @@ const faiz = () => {
     alert(`Kredi Tipi Seçimi Yapınız.`);
   }
   return faizOrani;
+}
+
+function virgul(x) {
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
+const hesapla = () => {
+  const vade = document.querySelector("#vade").value;
+  const tutar = document.querySelector("#tutar").value;
+  let tipi = document.querySelector("#krediTipi").value;
+  
+  let taksitTutari =
+    (tutar * ((faiz() / 100) * Math.pow(1 + faiz() / 100, vade))) /
+    (Math.pow(1 + faiz() / 100, vade) - 1).toFixed(2);
+
+  document.getElementById("krediMiktari").innerText = tutar + " TL";
+  document.getElementById("krediTipiTablo").innerText = tipi.toUpperCase();
+  document.getElementById("vadesi").innerText = vade;
+  document.getElementById("faizOrani").innerText = "% "+ faiz();
+  document.getElementById("taksitTutari").innerText =
+    virgul(taksitTutari.toFixed() + " TL");
+  document.getElementById("genelToplam").innerText =
+    virgul((taksitTutari * vade).toFixed() + " TL");
+  document.getElementById("plan").classList.remove("d-none")
 };
 
 buton.addEventListener("click", function (e) {
   e.preventDefault();
-  console.log(faiz());
+  hesapla();
+  buton.classList.remove("btn-warning")
+  buton.classList.add("btn-primary")
+  baslik.classList.remove("text-warning");
+  baslik.classList.add("text-primary");
 });
-
-const hesapla = () => {
-  let taksitTutari;
-};
 
 //*PSUDEO KOD;
 // değişken tanımlama
