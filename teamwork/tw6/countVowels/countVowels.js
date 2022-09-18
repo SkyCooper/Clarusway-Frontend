9fee14174b90874b24f26404ce8367b5b212a5b5
Cooper <dancersoftaranto@gmail.com>
2022-09-18 14:59:00 +0300

    wovels bg

--- teamwork/tw6/countVowels/countVowels.js
+++ teamwork/tw6/countVowels/countVowels.js
@@ -1,28 +1,26 @@
-
 const input = document.getElementById("input");
 console.log(input);
 
-const buton = document.querySelector(".btn")
+const buton = document.querySelector(".btn");
 console.log(buton);
-
 
 function countVowels() {
   return input.value
-    .split("")
-    .filter((x) => x == "a" || x == "e" || x == "i" || x == "o" || x == "u")
-    .length;
-  // return (str.match(/[aeiou]/gi) || []).length;
-  // return str.replace(/[^aeiou]/gi, "").length;
-  // return str.split("").filter((c) => "aeiouAEIOU".includes(c)).length;
+    .split("").filter((x) => x == "a" || x == "e" || x == "i" || x == "o" || x == "u").length;
+    
+  // return (input.value.match(/[aeiou]/gi) || []).length;
+  // return input.value.replace(/[^aeiou]/gi, "").length;
+  // return input.value.split("").filter((c) => "aeiouAEIOU".includes(c)).length;
 }
 
-buton.addEventListener("click", function(){
-  document.getElementById("result").innerText = 
-  `There are ${countVowels()} vowels in  :`
-  document.getElementById("sonuc").innerText = 
-  `"${input.value}"`
-})
+buton.addEventListener("click", function () {
+  document.getElementById(
+    "result"
+  ).innerText = `There are ${countVowels()} vowels in  :`;
+  document.getElementById("sonuc").innerText = `"${input.value}"`;
+  document.getElementsByTagName("body")[0].style.background = "green";
+  document.getElementsByClassName("vowels")[0].classList.add("image")
+});
 
 
-
-
+console.log(document.getElementsByClassName("vowels")[0]);
