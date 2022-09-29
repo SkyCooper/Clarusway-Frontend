02afa048cff66ad7765cb5ff8b2dc1d266ccd9b9
Cooper <dancersoftaranto@gmail.com>
2022-09-29 21:55:35 +0300

    ES6

--- frontend/javascript/sessions/s21_oop1/1-objectLiteral.js
+++ frontend/javascript/sessions/s21_oop1/1-objectLiteral.js
@@ -4,3 +4,37 @@
 
 //* Object Literals
 console.log("**** Object Literals ****");
+
+const book1 = {
+  title: "Simyacı",
+  author: "Paulo Coelho",
+  year: 1999,
+  getSummery: function () {
+    return `${this.title} written by ${this.author} in ${this.year}`;
+  },
+  //* arrow kullanılmıyır, çünkü this yanlış çalışıyor.
+};
+
+console.log(book1);
+//* konsolda görünen prototype bu objenin nereden türetildiğini gösterir.
+console.log(book1.getSummery());
+
+console.log(book1.hasOwnProperty("title")); // true
+//* book1 içinde title key'i var mı? Boolen feğer döndürür.
+//* hasOwnProperty() fonksiyonunu biz yazmadık, OBJECT klasından miras aldı.
+
+const book2 = {
+  title: "Animal Farm",
+  author: "George Orwell",
+  year: 1876,
+  getSummery: function () {
+    return `${this.title} written by ${this.author} in ${this.year}`;
+  },
+};
+
+console.log(book2);
+console.log(book2.getSummery());
+
+//? Object literals yontemi ile fazla sayida yeni objeler olusturmak oldukca zahmetlidir. Seri üretim için elverişli değildir. 
+//? Ayrica programcilik yaklasimi acisindan da cok tekrar icerdigi icin uygun degildir (DRY - Dont Repeat Yourself)
+//? Cozum: Object Oriented Programming (ES5 and ES6)
--- frontend/javascript/sessions/s21_oop1/2-objectContstructor.js
+++ frontend/javascript/sessions/s21_oop1/2-objectContstructor.js
@@ -1,5 +1,5 @@
 //* ======================================================
-//*             OOP - Object Constructor (ES5)
+//!             OOP - Object Constructor (ES5)
 //* ======================================================
 
 //* Javascript, prototype-temelli bir dildir.
@@ -9,3 +9,81 @@
 //* Ornegin Date ve Array nesneleri Object.prototype'dan miras almaktadir.
 
 //? Object Constructor
+
+//* constructor olduğunu belli etmek için baş harfini BÜYÜK yazdık
+function Book(t, author, year) {
+  //* this ile yazılan oluşacak obejenin keyidir, paramatre ismi ile aynı olmak zorunda değildir.
+  this.title = t;
+  (this.author = author),
+    (this.year = year),
+    (this.getSummery = function () {
+      return `${this.title} written by ${this.author} in ${this.year}`;
+    });
+}
+
+//? new keyword'u Book Constructor'ini parameterler ile cagirmaktadir.
+//? Constructor ise Book nesnesinden bir ornek (instance) olusturmaktadir.
+//? Constructor, mantiksal bir ifade iken instance fiziksel bir olusum gibi dusunulebilir.
+//? Contructor'da tanimlanmis tum degisken ve fonksiyonlar olusturulan her bir instance'da hayat bulmus olur.
+
+//* kalıp oluştu, seri üretime geçiyoruz.
+
+//* instance
+const book1 = new Book("Simyacı", "Paulo", 1999);
+book1.price = "100"; //* sadece book1 de oluşur, 2 de olmaz.
+console.log(book1);
+
+//* instance
+const book2 = new Book("Kasagi", "Omer", 1895);
+console.log(book2.getSummery());
+
+//! Prototype, belirli bir Nesne'nin (Object) tum instance'larina kolay bir sekilde metotlar tanimlamaya izin vermektedir.
+//! Prototip alaninda bir metot tanimlamanin avantaji bu metot'un olusan tum instance'larin belleginde yer kaplamamasi ancak tum instance'larin bu metota ulasabilmesidir.
+//? bütün instance'larda ortak olan şeyler prototype yazılmalı, mesela price her üründe farklı olabilir, onun için aslında constructor içine yazmak mantıklı
+//? fakat getAge gibi tarih hesaplaması hepsinde ortak olduğunda prototype içinde yazmak mantıklı
+
+//* yeni ilave yazılan fonksiyonu constructor içinde değil Genel alanda PROTOTYPE alanında yazdık.
+Book.prototype.getAge = function () {
+  return new Date().getFullYear() - this.year;
+};
+//* Ornegin Book nesnesinin tum instance'lari getAge() fonksiyonunu miras alabilir.
+//* Ancak, getAge() fonksiyonu bellekte sadece bir yer kaplamaktadir.
+
+//* prototype içine ilave bilgi de eklenebilir.
+Book.prototype.paper = "Color";
+
+console.log(book1, book2); //* prototype içinde görünür.
+console.log(book1.getAge()); // 23
+
+//* Bir nesnenin prototiplerine .prototype ile erisilebilir.
+//* Ancak bir instance'in prototiplerine .__proto__ ile erisilmektedir.
+
+console.log(Book.prototype); // Book objesinin prototypei
+console.log(book1.__proto__); // book1 instance'sinin prototypei
+
+//* ======================================================
+//!             INHERITANCE - Kalıtım (ES5)
+//* ======================================================
+
+//* book objesinde magazine üretilecek,
+//?Sub-Class
+
+function Magazine(title, author, year, month) {
+  Book.call(this, title, author, year);
+  //* magazine title, author, year bunları Book'tan çağırdı,
+  this.month = month; //* sonra yeni özellik eklendi
+  //* Book + mounth gibi birşey oldu.
+}
+
+//! Prototype miras almak için Object.create()metodu kullanılır.
+Magazine.prototype = Object.create(Book.prototype);
+
+//*instance
+const mag1 = new Magazine("Science", "Einttein", 2021, "September");
+console.log(mag1);
+console.log(mag1.getSummery()); // çalışır
+// console.log(mag1.getAge()); //  79.satır yorum satırı ise çalışmaz, çünkü prototypeda tanımlandı.
+
+//! Prototipler doğrudan miras olarak gelmez, create() yapmak lazım.
+console.log(mag1.getAge()); // 79.satır yorum değilse çalışır, çünkü prototypeda tanımlandı.
+console.log(mag1.paper); // Color olarak gelir.
